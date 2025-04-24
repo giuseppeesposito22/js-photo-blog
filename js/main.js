c15a740da8d9b5d38a91af551e7fb8e8b6a96320
giuseppeesposito22 <gius.espo22@gmail.com>
2025-04-24 18:08:40 +0200

    add rotate effect on card

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 //Elementi del DOM
 const rowEl = document.querySelector(".row");
+let cardsEl;
 
 //End point dell' API utilizzata
 const apiUri = `https://lanciweb.github.io/demo/api/pictures/`;
@@ -17,22 +18,37 @@ const generateElement = (apiUri) => {
 
     arrObj.forEach((obj) => {
       contentCard += ` <div class="col-12 col-md-6 col-xl-4">
-            <div class="card mx-auto" style="width: 18rem">
-              <img class="pin" src="./img/pin.svg" alt="" />
-               <img src="${obj.url}" alt="..." /> 
-              <div class="card-body">
-                <p class="card-text">
-                ${obj.date}
-                </p>
-                <p class="card-text fw-bold fs-3">
-                ${obj.title}
-                </p>
-              </div>
-            </div>
-          </div>`;
+      <div id="card" class="card mx-auto" style="width: 18rem">
+      <img class="pin" src="./img/pin.svg" alt="" />
+      <img src="${obj.url}" alt="..." />
+      <div class="card-body">
+      <p class="card-text">
+      ${obj.date}
+      </p>
+      <p class="card-text fw-bold fs-3">
+      ${obj.title}
+      </p>
+      </div>
+      </div>
+      </div>`;
     });
 
     rowEl.innerHTML = contentCard;
+
+    cardsEl = document.querySelectorAll(".card");
+
+    for (const card of cardsEl) {
+      const pin = card.querySelector(".pin");
+
+      card.addEventListener("mouseover", () => {
+        card.style.transform = "rotate(40deg)";
+        if (pin) pin.classList.add("d-none");
+      });
+      card.addEventListener("mouseout", () => {
+        card.style.transform = "rotate(0deg)";
+        if (pin) pin.classList.remove("d-none");
+      });
+    }
   });
 };
 
